fbabb19dfc2063894157d781708fea852e80d6b8
Patrick Perry <patperry@gmail.com>
2014-04-08 14:25:15 -0400

    Add glmm test

--- inst/tests/test-hglm-fast.R
+++ inst/tests/test-hglm-fast.R
@@ -39,3 +39,49 @@ test_that("succeeds on sleepstudy", {
     colnames(ranef0) <- c("(Intercept)", "Days")
     expect_that(round(ranef(model)[["Subject"]], 1), equals(ranef0))
 })
+
+
+test_that("succeeds on cbpp", {
+    model <- hglm.fast(cbind(incidence, size - incidence) ~ period, herd,
+                       data=cbpp, family=binomial)
+
+    # fixef
+    fixef0 <- c("(Intercept)" = -1.2, "period2" = -0.8, "period3" = -0.9,
+                "period4" = -1.1)
+    expect_that(round(fixef(model), 1), equals(fixef0))
+
+    # vcov
+    vcov0 <- matrix(c( 0.1, -0.1, -0.1, -0.1, -0.1, 0.2, 0.1, 0.0,
+                      -0.1,  0.1,  0.2,  0.0, -0.1, 0.0, 0.0, 0.1), 4, 4)
+    rownames(vcov0) <- colnames(vcov0) <- c("(Intercept)", "period2",
+                                            "period3", "period4")
+    expect_that(round(vcov(model), 1), equals(vcov0))
+
+    # VarCorr
+    varcor0 <- matrix(c( 0.7, -0.7, -0.4, -0.5, -0.7, 1.0, 1.0, 0.3,
+                        -0.4,  1.0,  1.4,  0.0, -0.5, 0.3, 0.0, 0.4), 4, 4)
+    rownames(varcor0) <- colnames(varcor0) <- c("(Intercept)", "period2",
+                                                "period3", "period4")
+    varcor <- VarCorr(model)[["herd"]]
+    expect_that(attr(varcor, "stddev"), equals(sqrt(diag(varcor))))
+    expect_that(as.vector(attr(varcor, "correlation")),
+                equals(as.vector(t(varcor / attr(varcor, "stddev"))
+                                 / attr(varcor, "stddev"))))
+    attr(varcor, "stddev") <- NULL
+    attr(varcor, "correlation") <- NULL
+    expect_that(round(varcor, 1), equals(varcor0))
+
+    # ranef
+    ranef0 <- matrix(c(-0.1, -0.4,  0.5,  0.1,  0.3, -0.2,  0.9,  0.5,  0.0, -0.7,
+                       -0.7,  0.0, -0.8,  1.2, -0.7,
+                        0.8,  0.2, -0.6,  0.2, -0.8, -0.1, -0.6, -0.5, -0.1,  0.3,
+                        1.0,  0.1,  0.7, -1.2,  0.7,
+                        1.5, -0.1, -0.6,  0.5, -1.2, -0.5,  0.2, -0.3, -0.3, -0.2,
+                        1.1,  0.1,  0.2, -0.8,  0.5,
+                       -0.3,  0.3, -0.3, -0.2,  0.1,  0.3, -0.8, -0.3,  0.1,  0.6,
+                        0.3,  0.0,  0.6, -0.7,  0.4),
+                     15, 4)
+    rownames(ranef0) <- as.character(1:15)
+    colnames(ranef0) <- c("(Intercept)", "period2", "period3", "period4")
+    expect_that(round(ranef(model)[["herd"]], 1), equals(ranef0))
+})
