2b9fa0a6087ba53d0d67d4862c43be20af02c52e
hacktuarial <tsweetser@gmail.com>
2016-03-05 11:03:06 -0800

    export all fns/methods

--- R/export.R
+++ R/export.R
@@ -5,7 +5,28 @@ fixef <- lme4::fixef
 fixef.mhglm <- lme4::fixef
 
 #' @export
+model.matrix.mhglm <- stats::model.matrix
+
+#' @export
 model.matrix <- stats::model.matrix
 
 #' @export
-predict.mhglm <- lme4::predict
+ranef.mhglm <- lme4::ranef
+
+#' @export
+ranef <- lme4::ranef
+
+#' @export
+sigma <- lme4::sigma
+
+#' @export
+sigma.mhglm <- lme4::sigma
+
+#' @export
+terms.mhglm <- stats::terms
+
+#' @export
+VarCorr <- lme4::VarCorr
+
+#' @export
+VarCorr.mhglm <- lme4::VarCorr
